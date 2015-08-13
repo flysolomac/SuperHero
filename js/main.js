3b1edc66557640ebe1bccfd9f41d843f1ee98ed5
flysolomac <macbaksh@gmail.com>
2015-08-13 18:32:57 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,115 +1,414 @@
-$(document).ready(function() 
-{
-    console.log ("starting super hero JS code");
+/*
+ *	set map variable for google map's map
+ *  set list of all markers to use later
+ */
+var map;
+allMarkers = [];
+
+/*
+ *	Main function to create google map
+ */
+function initialize() {
+  /*
+   * Set the variable for the starting point
+   */
+  var atlanta = new google.maps.LatLng(33.7461351, -84.389745);
+  /*
+   * Set the variable for the google map option
+   */
+  var mapLocate = {
+    zoom: 12,
+    center: atlanta,
+    disableDefaultUI: true
+  };
+  /*
+   * create a new map object
+   */
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapLocate);
+}
+
+/*
+ *	Main function to create and place markers on google map
+ * takes marker variable as a parameter
+ */
+function addGoogleMapsMarkers(m) {
+  // Display multiple markers on a map
+  var infoWindow = new google.maps.InfoWindow();
+
+  /*
+   * Function to create Info window for the google map marker
+   * Takes the marker data as a parameter
+   */
+  function makeInfoWindow(mk) {
+    /*
+     * Create the DOM element for the marker window
+     * Uses marker data to create Business name, phone number, reviewer's picture, and reviewer's review
+     */
+    var infoWindowContent = '<div class="info_content">';
+    infoWindowContent += '<h4>' + mk.title + '</h4>';
+    infoWindowContent += '<p>' + mk.ph + '</p>';
+    infoWindowContent += '<p class="review"><img src="' + mk.pic + '">' + mk.blurb + '</p>';
+    infoWindowContent += '</div>';
 
     /*
-    ** generally you want to declare your constants up top early so they are easy to find and change if need be
-    */
-    // creating these constants to index super heroes and make code easier to read and maintain
-    var Hulk = 0;
-    var Flash = 1;
-    var Superman = 2;
-    var Wolverine = 3;
-    var CaptainAmerica = 4;
-    var LastHero = CaptainAmerica;
+     * Google Map V3 method to set the content of the marker window
+     * Takes above infoWindowContent variable as a parameter
+     */
+    infoWindow.setContent(String(infoWindowContent));
 
-    // time to wait before alternating hero view
-    var HeroChangeTimeoutMS = 3000;
-    
-    var SuperHero = function(img,name,about,truename,power,enemy,color)
-    {
-        
-        this.img = img;
-        this.name= name;
-        this.about= about;
-        this.truename= truename;
-        this.power= power;
-        this.enemy= enemy;
-        this.color=color;
-        // this method returns true if the hero has XRay Vision as one of its super powers; false otherwise
-        this.hasXrayVision = function (){
-            numPowers = this.power.length;
-            var hasXrayVision = false;
-               for (var j=0; j < numPowers; j++)
-                {
-                    if ( this.power[j] == " Xray Vision")
-                    {
-                        hasXrayVision = true;
-                    }
-                }   
-                return hasXrayVision;
-            }
-    };
-    
-    
-    // Will Display Text Green If hero has XrayVision , Red otherwise
-    function xrayVisionID (hasXrayVision) {
-         
-        if (hasXrayVision === false)
-            {
-                $("#xray").text("False");
-                $("#xray").css("color","red");
-            }
-                else
-                {
-                    $("#xray").text("True");
-                    $("#xray").css("color","green");
-                }
-
-                
-            };
-
-    // moved changeHero out of the SuperHero constructor. This function is not a method for a particular hero so we want it to be 
-    // outside of that
-        function changeHero (hero)
-    {
-        console.log ("changing super hero view to " + hero.name);
-        $("#heroImg").attr("src",hero.img);
-        $("#heroName").text(hero.name);
-        $("#info").text(hero.about);
-        $("#truename").text(hero.truename);
-        $("#power").text(hero.power);
-        $("#enemy").text(hero.enemy);
-        $("#heroName").css("color",hero.color);
-        xrayVisionID(hero.hasXrayVision())
-        console.log ("created super hero " + hero.name);
-         
-    }
-    
-    function changeHeroViewByIndex (index)
-    {
-        changeHero (superHeros [index]);
-    }
-    
- 
-    
-    var superHeros = new Array();
-            superHeros [Hulk] = new SuperHero("img/hulk.jpg","Hulk","Super strong genetically muatated army experiment.","Bruce Banner",["Super Strength"," Can grow in size"," Anger boost strength"],"Military","Green");
-            superHeros [Flash] = new SuperHero("img/flash.jpg","Flash","Struck by lightning and inherited the ability of super speed.","Barry Allen",["Super Speed"," Go through walls"," Reverse time"],"Fiddler","Red");
-            superHeros [Superman] = new SuperHero("img/superman.jpg","Superman","Alien Human from another planet.","Clark Kent",["Super Human Abilities"," Xray Vision"," Laser Beams"],"Lex Lugor","Blue");
-            superHeros [Wolverine] = new SuperHero("img/wolverine.jpg","Wolverine","Genetic experiment infused with the world's strongest metal.","James (Logan) Howlet",["Regeneration"," Metal Claws"," Great Strength"],"Magneto","Yellow");
-            superHeros [CaptainAmerica] = new SuperHero("img/captain.jpg","Captain America","Army enhanced super soldier.","Joe Simon",["Super Soldier"," Power Shield"," Justice"],"Red Skull","Blue");
-           
     /*
-    ** this function will iterate through each of the super heros in the array and will
-    ** stop once all super heros in the array have been displayed
-    */
-    // using 'j' as an index variable as its much easier to find vs. 'i'
-    for (var j=0; j < superHeros.length; j++)
-    {
-        // need to multiply timeout by index (j) or else all values chagne at the same time and you only see last super hero
-        var timeoutValue = j * HeroChangeTimeoutMS;
+     * Google Map V3 method to set the content of the marker window
+     * Takes map and marker data variable as a parameter
+     */
+    infoWindow.open(map, mk);
+  }
 
-        // the method below works but not for IE9 or below
-        // setTimeout(changeHero, timeoutValue, superHeros[j]);
+  /*
+   * Function delete all markers on the map
+   */
+  function deleteAllMarkers() {
+    /*
+     * Loops over all the markers on the map and use the google map method .setMap(null) to remove it
+     */
+    for (var i = 0, max = allMarkers.length; i < max; i++) {
+      allMarkers[i].setMap(null);
+    }
+    /*
+     * clears the allMarkers variable
+     */
+    allMarkers = [];
+  }
 
-        // the method below works but is less efficient than the one above
-        setTimeout(function(x) { return function() { changeHero(superHeros[x]); }; }(j), timeoutValue);
+  /*
+   * if all Markers variable contains any markers object, call the deleteAllMarkers function to remove it.
+   */
+  if (allMarkers.length > 0) {
+    deleteAllMarkers();
+  }
 
+  /*
+   * Loop through our array of markers & place each one on the map
+   */
+  for (var i = 0, max = m.length; i < max; i++) {
+    // create the position object
+    var position = new google.maps.LatLng(m[i][2], m[i][3]);
+    // create the mkr object from the marker param
+    var mkr = new google.maps.Marker({
+      position: position,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      title: m[i][0],
+      ph: m[i][1],
+      pic: m[i][4],
+      blurb: m[i][5]
+    });
+    // update allMarkers array variable with mkr object
+    allMarkers.push(mkr);
 
-    };
-    
+    /*
+     * Apply google maps event method to bind a mouseover event to the marker
+     * on event, create and show info window using the makeInfoWindow Method
+     */
+    google.maps.event
+      .addListener(mkr, 'mouseover', (function(mk, i) {
+        return function() {
+          makeInfoWindow(mk);
+        };
+      })(mkr, i));
 
-    console.log ("exiting super hero JS now... ok");
-   
-}); // end of file
+    /*
+     * Apply google maps event method to bind a mouse click event to the marker
+     * on event, create and show info window using the makeInfoWindow Method
+     * and animate the marker
+     */
+    google.maps.event
+      .addListener(mkr, 'click', (function(mk, i) {
+        return function() {
+          makeInfoWindow(mk);
+          toggleBounce(mk, i);
+        };
+      })(mkr, i));
+  }
+
+  /*
+   * Function to animate the marker
+   */
+  function toggleBounce(mk, i) {
+    /*
+     * Create the variable
+     */
+    var yelpMarkerDetailUl = $('.yelp-list').find('ul'),
+      yelpMarkerDetail = yelpMarkerDetailUl.find('li'),
+      yelpMarkerDetailPos = 212 * i,
+      activeYelpMarkerDetail = yelpMarkerDetail.eq(i);
+
+    /*
+     * If the marker has animation attribute
+     * then remove the animation attribute
+     * also remove the show className from the yelp-list ul dom to slide left
+     * also remove the active className from the active yelp-list ul li dom
+     */
+    if (mk.getAnimation() !== null) {
+      /*  mk.setAnimation(null);
+	    yelpMarkerDetailUl.removeClass('show');
+	    activeYelpMarkerDetail.removeClass('active');*/
+      /*
+       * If marker does not have animation attribue
+       * remove animation attribute from any other markers that are animated
+       * then set the animation attribute to the clicked marker
+       * also add the show className from the yelp-list ul dom to slide right
+       * also add the active className to the yelp-list ul li dom
+       */
+    } else {
+      for (var am in allMarkers) {
+        // iterate through all the markers and see if it has the animation attribute
+        var isMoving = allMarkers[am].getAnimation();
+        /*
+         * if marker is animating and index is not self
+         * then set the animated marker's animation attribute to null
+         */
+        if (isMoving && am !== i) {
+          allMarkers[am].setAnimation(null);
+        }
+      }
+
+      /*
+       * add the Bounce animation to the clicked marker
+       * using google map's animation method
+       * also add the show className from the yelp-list ul dom to slide right and animate the child dom to the top
+       * also add the active className to the yelp-list ul li dom
+       */
+      mk.setAnimation(google.maps.Animation.BOUNCE);
+      yelpMarkerDetailUl.addClass('show').animate({
+        scrollTop: yelpMarkerDetailPos
+      }, 300);
+      yelpMarkerDetailUl.find('.active').removeClass('active');
+      activeYelpMarkerDetail.addClass('active');
+    }
+  }
+
+  /*
+   * add click event to the yelp-list ul li dom
+   */
+  $('.results').find('li').click(function() {
+    // get index of clicked element
+    var pos = $(this).index();
+    // iterate through allMarkers array
+    for (var am in allMarkers) {
+      var isMoving = allMarkers[am].getAnimation();
+      // if marker is animated, remove animation
+      if (isMoving && am !== pos) {
+        allMarkers[am].setAnimation(null);
+      }
+    }
+
+    /*
+     * add the Bounce animation to the marker that corresponding to the clicked element index
+     * using google map's animation method, create and show the info window
+     * also remove the active className from the active yelp-list ul li dom
+     * then add the active className to the clicked element
+     */
+    allMarkers[pos].setAnimation(google.maps.Animation.BOUNCE);
+    makeInfoWindow(allMarkers[pos]);
+    $('.results').find('.active').removeClass('active');
+    $(this).addClass('active');
+  });
+}
+
+/*
+ *	This is the main function that calls to yelp
+ * 	and updates the knockout data binds
+ *	as well as creating the  markers on google map.
+ */
+function yelpCall(searchNear, searchFor) {
+  /*
+   *	Details for Yelp OAuth2
+   *	This info would never make it to prod - it's only used right now as Proof-of-concept
+   */
+  var auth = {
+    consumerKey: "DkEkxhVsYxtxev2cKEcv3Q",
+    consumerSecret: "N9NMg4RvC6e2M36HJN69l7a8WHI",
+    accessToken: "8cWDzJzc80D7xxhcMpO7GrDlq9i_lIAj",
+    accessTokenSecret: "w973vYGixNR_EhxRtUNGcXrfdko",
+    serviceProvider: {
+      signatureMethod: "hmac-sha1"
+    }
+  };
+
+  /*
+   *	Create a variable "accessor" to pass on to OAuth.SignatureMethod
+   */
+  var accessor = {
+    consumerSecret: auth.consumerSecret,
+    tokenSecret: auth.accessTokenSecret
+  };
+
+  /*
+   *	Create a array object "parameter" to pass on "message" JSON object
+   */
+  var parameters = [];
+  parameters.push(['term', searchFor]);
+  parameters.push(['location', searchNear]);
+  parameters.push(['callback', 'cb']);
+  parameters.push(['oauth_consumer_key', auth.consumerKey]);
+  parameters.push(['oauth_consumer_secret', auth.consumerSecret]);
+  parameters.push(['oauth_token', auth.accessToken]);
+  parameters.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+  /*
+   *	Create a JSON object "message" to pass on to OAuth.setTimestampAndNonce
+   */
+  var message = {
+    'action': 'http://api.yelp.com/v2/search',
+    'method': 'GET',
+    'parameters': parameters
+  };
+
+  /*
+   *	OAuth proof-of-concept using JS
+   */
+  OAuth.setTimestampAndNonce(message);
+  OAuth.SignatureMethod.sign(message, accessor);
+
+  /*
+   *	OAuth proof-of-concept using JS
+   */
+  var parameterMap = OAuth.getParameterMap(message.parameters);
+  yAjax(message.action, parameterMap);
+}
+
+/*
+ *	Ajax OAuth method to get yelp's data
+ */
+function yAjax(url, ydata) {
+  $.ajax({
+    'url': url,
+    'data': ydata,
+    'dataType': 'jsonp',
+    'global': true,
+    'jsonpCallback': 'cb',
+    'success': function(data) {
+      makeYelpList(data);
+    }
+  });
+}
+
+/*
+ *	Function to create the list from Yelp's API
+ *	takes returned data from the ajax as a parameter
+ */
+function makeYelpList(d) {
+  /*
+   *	Create the variable 
+   */
+  var $yelpList = $('.results');
+  results = d.businesses,
+    el = '';
+
+  /*
+   *	Clear the yelpList to add new entries
+   */
+  $yelpList.empty();
+
+  /*
+   *	Create the markers Array object
+   */
+  var markers = [];
+
+  /*
+   *	If no data is returned
+   */
+  if (results.length > 0) {
+    /*
+     *	loop through the returned data
+     *	then create the variable for to use in populating the yelp-list li Dom
+     */
+    for (var result in results) {
+      var business = results[result],
+        name = business.name,
+        img = business.image_url,
+        ph = business.display_phone,
+        url = business.url,
+        stars = business.rating_img_url_small,
+        rate = business.rating,
+        loc = {
+          lat: business.location.coordinate.latitude,
+          lon: business.location.coordinate.longitude,
+          address: business.location.display_address[0] + '<br>' + business.location.display_address[business.location.display_address.length - 1]
+        },
+        review = {
+          img: business.snippet_image_url,
+          txt: business.snippet_text
+        };
+
+      /*
+       *	create the Dom object
+       */
+      var makeEl = '<li><div class="heading row"><p class="col-sm-3 img-container">';
+      makeEl += '<img src="' + img + '" height=125 width=125 class="img-thumbnail">';
+      makeEl += '<img src="' + stars + '" height=17 width=84 alt="Yelp Rating">';
+      makeEl += '</p><div class="col-sm-9">';
+      makeEl += '<h3>' + name + '</h3><p>';
+      makeEl += '<span>' + loc.address + '</span></p>';
+      makeEl += '<p><strong>' + ph + '</strong></p>';
+      makeEl += '<p><a class="btn btn-default btn-large" href="' + url + '" target="_blank">Yelp it!</a></p>';
+      makeEl += '</div></div></li>';
+      
+      var src = $('.image-thumbnail').attr("src");
+      console.log(src);
+      if (src === undefined || src === null){
+    /*
+     *	If img source is returned undefined 
+     */
+
+   $('.image-thumbnail').attr("src", "http://tinyurl.com/imgFail");
+                   
+
+   } 
+
+      /*
+       *	add to the el variable
+       */
+      el += makeEl;
+
+      /*
+       *	create the marker array object
+       *	then add marker to the markers array object
+       */
+      var marker = [name, ph, loc.lat, loc.lon, review.img, review.txt];
+      markers.push(marker);
+    }
+    /*
+     *	add the el to the yelp-list ul dom
+     */
+    $yelpList.append(el);
+
+    /*
+     *	Use google map api to create the markers to place on the map
+     */
+    google.maps.event.addDomListener(window, 'load', addGoogleMapsMarkers(markers));
+
+    /*
+     *	If no data is returned,
+     *	then create a error message
+     */
+  }
+else {
+    var searchedFor = $('input').val();
+    $yelpList.addClass('open').append('<li><h3>Oops! We can\'t find anything on <span>' + searchedFor + '</span>.</h3><p>Search something else.</p></li>');
+    /*
+     *	Use google map api to clear the markers on the map
+     */
+    google.maps.event.addDomListener(window, 'load', addGoogleMapsMarkers(markers));
+  }
+}
+
+/*
+ * initialize the google maps function
+ */
+initialize();
+
+/*
+ * Call the main yelp function
+ */
+yelpCall("30308", "Wendy's");
